e33044f7f4bbe4afe3000cbcbb32ff2bf8278815
Eduardo Hoffmam <eduhoffmam@gmail.com>
2024-11-24 21:27:16 -0300

    pequenas mudanças no @media

--- FilaOnline/View/Usuario/Cadastro.php
+++ FilaOnline/View/Usuario/Cadastro.php
@@ -10,6 +10,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
         <?php
